088de4da391d414157638a85b68dc53d73118b3f
asheesh1510 <124050531+asheesh1510@users.noreply.github.com>
2026-02-19 13:02:44 +0530

    Change database from DEV_EDW to TST_EDW

--- Sigma_Streamlit.py
+++ Sigma_Streamlit.py
@@ -6,13 +6,13 @@ conn = snowflake.connector.connect(
     password="Osi@Digital!@34",
     account="MWDVYOC-BS73011",
     warehouse="COMPUTE_WH",
-    database="DEV_EDW",
+    database="TST_EDW",
     schema="BASE"
 )
 
 
 cur = conn.cursor()
-cur.execute("SELECT * FROM USERS LIMIT 10")
+cur.execute("SELECT * FROM TST_EDW.BASE.USERS LIMIT 10")
 data = cur.fetchall()
 
 st.write(data)
